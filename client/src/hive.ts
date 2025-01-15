f5ee2495a5aa6d9307b6ac733bc70c759cbc5e38
Anh Tài <anhtai05102002@gmail.com>
2025-01-16 03:12:23 +0700

    Update Hive.

--- client/src/hive.ts
+++ client/src/hive.ts
@@ -1,75 +1,105 @@
 const HexWidth = 200;
-const HexHeight = 174;
+const HexHeight = 173;
+const MidHeight = 87;
+
+// const points = "100,0 50,87 -50,87 -100,0 -50,-87 50,-87";
+const points = "98,0 48,85 -48,85 -98,0 -48,-85 48,-85";
 
 class Hive {
-  hexs: Hex[];
-  x: number = 0;
-  y: number = 0;
-  width: number = 0;
-  height: number = 0;
+  rotate: string = "0";
+  pieces: Piece[] = [];
+  selected: string | null = null;
+  moves: string[] = [];
+
   constructor() {
-    this.hexs = [
-      new Hex(0, 0, 0),
-      new Hex(1, 0, -1),
-      new Hex(1, -1, 0),
-      new Hex(0, -1, 1),
-      new Hex(-1, 0, 1),
-      new Hex(-1, 1, 0),
-      new Hex(0, 1, -1),
-      new Hex(2, 0, -2),
-      new Hex(2, -1, -1),
-      new Hex(2, -2, 0),
-      new Hex(1, -2, 1),
-      new Hex(0, -2, 2),
-      new Hex(-1, -1, 2),
-      new Hex(-2, 0, 2),
-      new Hex(-2, 1, 1),
-      new Hex(-2, 2, 0),
-      new Hex(-1, 2, -1),
-      new Hex(0, 2, -2),
-      new Hex(1, 1, -2),
-    ];
-    this.update();
+    this.pieces = Piece.init();
   }
 
-  add(hex: Hex) {
-    if (this.hexs.find((h) => h.x === hex.x && h.y === hex.y && h.z === hex.z)) {
-      console.log("hex already exists");
-      return;
+  get targets(): Hex[] {
+    if (!this.selected) {
+      console.error("Piece not selected yet!");
+      return [];
     }
-
-    this.hexs.push(hex);
-    this.update();
-  }
-
-  addRandom() {
-    this.add(new Hex(3, 0, -3));
-    this.add(new Hex(0, 3, -3));
-    this.add(new Hex(0, -3, 3));
-    this.add(new Hex(-3, 0, 3));
-  }
-
-  addAll(hexs: Hex[]) {
-    this.hexs = this.hexs.concat(hexs);
-    this.update();
-  }
-
-  update() {
-    const minTX = Math.min(...this.hexs.map((hex) => hex.tX));
-    const maxTX = Math.max(...this.hexs.map((hex) => hex.tX));
-    const minTY = Math.min(...this.hexs.map((hex) => hex.tY));
-    const maxTY = Math.max(...this.hexs.map((hex) => hex.tY));
-
-    console.log(minTX, maxTX, minTY, maxTY);
-
-    this.x = minTX - HexWidth * 1.5;
-    this.y = minTY - HexHeight * 1.5;
-    this.width = maxTX - minTX + HexWidth * 3;
-    this.height = maxTY - minTY + HexHeight * 3;
+    const piece = this.pieces.find((p) => p.id === this.selected);
+    if (!piece) {
+      console.error("Can't find piece!");
+      return [];
+    }
+    // If split the hive => can't move;
+    // If just move => can't move;
+    // If can't slide out => can't move;
+    // Can move oppoment's piece if there's a Pillbug nearby;
+    return [];
   }
 
   get viewBox() {
-    return `${this.x} ${this.y} ${this.width} ${this.height}`;
+    let left = Infinity;
+    let right = -Infinity;
+    let up = Infinity;
+    let down = -Infinity;
+    if (this.rotate == "flat") {
+      this.pieces.forEach((p) => {
+        const x = p.hex.x;
+        if (x < left) left = x;
+        if (x > right) right = x;
+        const minus = p.hex.y - p.hex.z;
+        if (minus < up) up = minus;
+        if (minus > down) down = minus;
+      });
+      const x = left * HexWidth * 0.75 - HexWidth * 1.25;
+      const y = up * MidHeight - HexHeight * 1.5;
+      const width = (right - left + 1) * HexWidth * 0.75 + HexWidth * 1.75;
+      const height = (down - up + 1) * MidHeight + HexHeight * 2.5;
+      return `${x} ${y} ${width} ${height}`;
+    }
+    // pointy
+    else {
+      this.pieces.forEach((p) => {
+        const minus = p.hex.x - p.hex.z;
+        if (minus < left) left = minus;
+        if (minus > right) right = minus;
+        const y = p.hex.y;
+        if (y < up) up = y;
+        if (y > down) down = y;
+      });
+      const x = left * MidHeight - HexHeight * 1.5;
+      const y = up * HexWidth * 0.75 - HexWidth * 1.25;
+      const width = (right - left + 1) * MidHeight + HexHeight * 2.5;
+      const height = (down - up + 1) * HexWidth * 0.75 + HexWidth * 1.75;
+      return `${x} ${y} ${width} ${height}`;
+    }
+  }
+}
+
+class Piece {
+  id: string; // <<w b>> <<A B G L M P Q S>> <<1 2 3 null>>
+  hex: Hex;
+  inBattle: boolean;
+
+  constructor(id: string, hex: Hex, inBattle: boolean = false) {
+    this.id = id;
+    this.hex = hex;
+    this.inBattle = inBattle;
+  }
+
+  static init(): Piece[] {
+    return [
+      new Piece("wA1", new Hex(0, 0, 0)),
+      new Piece("wA2", new Hex(1, -1, 0)),
+      new Piece("wA3", new Hex(1, 0, -1)),
+      new Piece("wB1", new Hex(-1, 1, 0)),
+      new Piece("wB2", new Hex(-1, 0, 1)),
+      new Piece("wG1", new Hex(0, 1, -1)),
+      new Piece("wG2", new Hex(0, -1, 1)),
+      new Piece("wG3", new Hex(2, 0, -2)),
+      new Piece("wL", new Hex(3, 0, -3)),
+      new Piece("wM", new Hex(3, 1, -4)),
+      // new Piece("wP", new Hex(0, 0, 0)),
+      // new Piece("wQ", new Hex(0, 0, 0)),
+      // new Piece("wS1", new Hex(0, 0, 0)),
+      // new Piece("wS2", new Hex(0, 0, 0)),
+      // new Piece("wA3", new Hex(0, 0, 0)),
+    ];
   }
 }
 
@@ -85,8 +115,13 @@ class Hex {
     this.y = y;
     this.z = z;
     this.tX = x * HexWidth * 0.75;
-    this.tY = (y - z) * HexHeight * 0.5;
+    this.tY = (y - z) * MidHeight;
+  }
+
+  compare(hex: Hex): boolean {
+    return this.x === hex.x && this.y === hex.y && this.z === hex.z;
   }
 }
 
 export default Hive;
+export { points };
--- client/src/views/Hive.vue
+++ client/src/views/Hive.vue
@@ -1,25 +1,103 @@
 <template>
-  <div class="board" @click="hive.addRandom()">
-    <svg :viewBox="hive.viewBox" width="100%" height="100%">
-      <g v-for="(hex, index) in hive.hexs" :key="index" :transform="`translate(${hex.tX}, ${hex.tY})`">
-        <polygon points="100,0 50,87 -50,87 -100,0 -50,-87 50,-87" fill="brown"></polygon>
-      </g>
-    </svg>
+  <div class="game" :class="landscape" :style="{ height: gameHeight }">
+    <div class="board">
+      <svg :viewBox="hive.viewBox" width="100%" height="100%">
+        <g style="transition: 0.2s" :class="hive.rotate">
+          <g
+            v-for="piece in hive.pieces"
+            :key="piece.id"
+            :transform="`translate(${piece.hex.tX}, ${piece.hex.tY})`"
+            @click="hive.selected = piece.id"
+          >
+            <polygon :points="points" fill="brown" :class="{ selected: piece.id === hive.selected }"></polygon>
+          </g>
+          <g>
+            <polygon :points="points" fill="none" stroke="pink" stroke-width="4"></polygon>
+          </g>
+          <g v-if="hive.selected" v-for="(hex, index) in hive.targets" :key="index"></g>
+        </g>
+      </svg>
+    </div>
+    <div class="panel">
+      <div class="rotate">
+        <a class="btn" @click="changeHiveOrientation()"><i class="fa-solid fa-arrow-left"></i></a>
+        <a class="btn">Rotate</a>
+        <a class="btn" @click="changeHiveOrientation()"><i class="fa-solid fa-arrow-right"></i></a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import Hive from "@/hive";
+import { onMounted, ref } from "vue";
+import Hive, { points } from "@/hive";
 
 const hive = ref(new Hive());
+const landscape = ref<string>("");
+const gameHeight = ref<string>("0px");
+
+function changeHiveOrientation() {
+  if (hive.value.rotate == "flat") {
+    hive.value.rotate = "pointy";
+  } else {
+    hive.value.rotate = "flat";
+  }
+}
+
+onMounted(() => {
+  landscape.value = window.innerWidth < window.innerHeight ? "" : "landscape";
+  gameHeight.value = `${window.innerHeight - 80}px`;
+  window.addEventListener("resize", () => {
+    gameHeight.value = `${window.innerHeight - 80}px`;
+    if (window.innerWidth < window.innerHeight) {
+      landscape.value = "";
+    } else {
+      landscape.value = "landscape";
+    }
+  });
+});
 </script>
 
 <style scoped>
+.game {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 .board {
-  height: calc(100vh - 80px);
+  height: 100%;
+  flex: 1;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.selected {
+  stroke: cyan;
+  stroke-width: 4;
+}
+
+.panel {
+  height: 15%;
+  width: 100%;
+  background-color: #333;
+}
+
+.rotate {
+  display: flex;
+}
+
+.landscape {
+  flex-direction: row;
+}
+
+.landscape .panel {
+  height: 100%;
+  width: 20%;
+}
+
+.pointy {
+  rotate: -30deg;
 }
 </style>
