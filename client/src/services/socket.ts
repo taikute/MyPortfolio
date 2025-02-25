f433f7b26f6ef709d597b8634f6627e735ff059b
Anh Tài <anhtai05102002@gmail.com>
2025-02-26 03:26:26 +0700

    Update

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -2,7 +2,7 @@ import { io, Socket } from "socket.io-client";
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   // "http://localhost:3000",
-  "ice-wss.up.railway.app",
+  "https://ice-wss.up.railway.app/",
   { autoConnect: false }
 );
 
