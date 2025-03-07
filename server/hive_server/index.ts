b86ee81d4d2c5ea08cd3ebb8eb3bb48fd1d54248
Anh Tài <anhtai05102002@gmail.com>
2025-03-07 07:33:46 +0700

    Test hive.

--- server/hive_server/index.ts
+++ server/hive_server/index.ts
@@ -1,1 +1,12 @@
-   
+import { log, error } from "console";
+import { randomUUID } from "crypto";
+import { Server } from "socket.io";
+import { setInterval } from "timers";
+
+const io = new Server(3000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+log("Host https://hive-socket.up.railway.app/");
