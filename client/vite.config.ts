0258fed5a015213b76ebacda34507cb39a979172
Anh Tài <anhtai05102002@gmail.com>
2025-03-25 13:27:33 +0700

    Test pwa.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,32 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		VitePWA({
-			registerType: "prompt",
-			manifest: {
-				name: "My Vue PWA Game",
-				short_name: "Game",
-				start_url: "/",
-				display: "standalone",
-				background_color: "#ffffff",
-				theme_color: "#42b983",
-				icons: [
-					{
-						src: "/pwa-icon-192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "/pwa-icon-512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-				],
-			},
-		}),
-	],
+	plugins: [vue(), VitePWA()],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
